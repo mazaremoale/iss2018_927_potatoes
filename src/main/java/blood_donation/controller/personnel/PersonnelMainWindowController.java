f0e41eeae87f92a226055bdb9ed16ef21acea441
ana <ana.spoiala25@gmail.com>
2018-06-04 22:08:12 +0300

    Functionalities for Personnel added : ui, login, display appointments

--- src/main/java/blood_donation/controller/personnel/PersonnelMainWindowController.java
+++ src/main/java/blood_donation/controller/personnel/PersonnelMainWindowController.java
@@ -1,16 +1,346 @@
 package blood_donation.controller.personnel;
 
+import blood_donation.domain.blood.Blood;
+import blood_donation.domain.blood.BloodGroup;
+import blood_donation.domain.people.Patient;
+import blood_donation.domain.people.Personnel;
+import blood_donation.domain.utils.*;
+import blood_donation.repository.Repository;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
+import org.hibernate.Session;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class PersonnelMainWindowController implements Initializable
 {
 
+    private Stage primaryStage;
+    private Session session;
+    private Scene previousScene;
+
+    private Personnel currentPersonnel;
+
+    private Repository<Donation> donationRepository;
+    private Repository<DonationRequest> donationRequestRepository;
+    private Repository<Clinic> clinicRepository;
+    private Repository<Blood> bloodRepository;
+    private Repository<BloodGroup> bloodGroupRepository;
+    private Repository<Distance> distanceRepository;
+    private Repository<Patient> patientRepository;
+    private Repository<DonationAppointment> donationAppointmentRepository;
+
+    @FXML
+    private Button backButton;
+    @FXML
+    private Button refreshButton;
+    @FXML
+    private Button checkAvailableStocksButton;
+    @FXML
+    private Button notifyNearestAvailableDonorsButton;
+    @FXML
+    private Button sendDonationRequestButton;
+    @FXML
+    private Button donorMedicalDataButton;
+    @FXML
+    private Button cancelDonationRequestButton;
+
+    @FXML
+    private TableView<DonationAppointment> donationRequestsTableView;
+    @FXML
+    private TableColumn<DonationAppointment, String> donationRequestDonorFirstNameTableColumn;
+    @FXML
+    private TableColumn<DonationAppointment, String> donationRequestDonorLastNameTableColumn;
+    @FXML
+    private TableColumn<DonationAppointment, String> donationRequestAppointmentDateTableColumn;
+    @FXML
+    private TableColumn<DonationAppointment, String> donationRequestAppointmentHourTableColumn;
+    @FXML
+    private TableColumn<DonationAppointment, String> donationRequestStatusTableColumn;
+
+    @FXML
+    private TableView bloodRequestsTableView;
+    @FXML
+    private TableColumn bloodRequestsDoctorNameTableColumn;
+    @FXML
+    private TableColumn bloodRequestsPatientNameTableColumn;
+    @FXML
+    private TableColumn bloodRequestsPriorityTableColumn;
+    @FXML
+    private TableColumn bloodRequestsRequestedBloodTableColumn;
+    @FXML
+    private TableColumn bloodRequestsStatusTableColumn;
+
+    @FXML
+    private TableView stocksTableView;
+    @FXML
+    private TableColumn stocksTypeTableColumn;
+    @FXML
+    private TableColumn stocksBloodGroupTableColumn;
+    @FXML
+    private TableColumn stocksRHTableColumn;
+    @FXML
+    private TableColumn stocksQuantityTableColumn;
+    @FXML
+    private TableColumn stocksDonationDateTableColumn;
+    @FXML
+    private TableColumn stocksExpirationDateTableColumn;
+    @FXML
+    private TableColumn stocksLocationTableColumn;
+
+    @FXML
+    private TableView journeyBloodInTestingTableView;
+    @FXML
+    private TableColumn journeyTestingStatusTableColumn;
+
+    @FXML
+    private TableView pendingDonationsTableView;
+    @FXML
+    private TableColumn pendingDonationsDonorNameTableColumn;
+    @FXML
+    private TableColumn pendingDonationsDonationDateTableColumn;
+
+
+    public Stage getPrimaryStage()
+    {
+        return primaryStage;
+    }
+
+    public PersonnelMainWindowController setPrimaryStage(Stage primaryStage)
+    {
+        this.primaryStage = primaryStage;
+        return this;
+    }
+
+    public Session getSession()
+    {
+        return session;
+    }
+
+    public PersonnelMainWindowController setSession(Session session)
+    {
+        this.session = session;
+        return this;
+    }
+
+    public Scene getPreviousScene()
+    {
+        return previousScene;
+    }
+
+    public PersonnelMainWindowController setPreviousScene(Scene previousScene)
+    {
+        this.previousScene = previousScene;
+        return this;
+    }
+
+    public Personnel getCurrentPersonnel()
+    {
+        return currentPersonnel;
+    }
+
+    public PersonnelMainWindowController setCurrentPersonnel(Personnel currentPersonnel)
+    {
+        this.currentPersonnel = currentPersonnel;
+        return this;
+    }
+
+    public Repository<Donation> getDonationRepository()
+    {
+        return donationRepository;
+    }
+
+    public PersonnelMainWindowController setDonationRepository(Repository<Donation> donationRepository)
+    {
+        this.donationRepository = donationRepository;
+        return this;
+    }
+
+    public Repository<DonationRequest> getDonationRequestRepository()
+    {
+        return donationRequestRepository;
+    }
+
+    public PersonnelMainWindowController setDonationRequestRepository(Repository<DonationRequest> donationRequestRepository)
+    {
+        this.donationRequestRepository = donationRequestRepository;
+        return this;
+    }
+
+    public Repository<Clinic> getClinicRepository()
+    {
+        return clinicRepository;
+    }
+
+    public PersonnelMainWindowController setClinicRepository(Repository<Clinic> clinicRepository)
+    {
+        this.clinicRepository = clinicRepository;
+        return this;
+    }
+
+    public Repository<Blood> getBloodRepository()
+    {
+        return bloodRepository;
+    }
+
+    public PersonnelMainWindowController setBloodRepository(Repository<Blood> bloodRepository)
+    {
+        this.bloodRepository = bloodRepository;
+        return this;
+    }
+
+    public Repository<BloodGroup> getBloodGroupRepository()
+    {
+        return bloodGroupRepository;
+    }
+
+    public PersonnelMainWindowController setBloodGroupRepository(Repository<BloodGroup> bloodGroupRepository)
+    {
+        this.bloodGroupRepository = bloodGroupRepository;
+        return this;
+    }
+
+    public Repository<Distance> getDistanceRepository()
+    {
+        return distanceRepository;
+    }
+
+    public PersonnelMainWindowController setDistanceRepository(Repository<Distance> distanceRepository)
+    {
+        this.distanceRepository = distanceRepository;
+        return this;
+    }
+
+    public Repository<Patient> getPatientRepository()
+    {
+        return patientRepository;
+    }
+
+    public PersonnelMainWindowController setPatientRepository(Repository<Patient> patientRepository)
+    {
+        this.patientRepository = patientRepository;
+        return this;
+    }
+
+    public Repository<DonationAppointment> getDonationAppointmentRepository()
+    {
+        return donationAppointmentRepository;
+    }
+
+    public PersonnelMainWindowController setDonationAppointmentRepository(Repository<DonationAppointment> donationAppointmentRepository)
+    {
+        this.donationAppointmentRepository = donationAppointmentRepository;
+        return this;
+    }
+
+
+    @FXML
+    public void goBack()
+    {
+        primaryStage.setScene(previousScene);
+    }
+
+    @FXML
+    public void refresh()
+    {
+        // re-populate all tables views
+    }
+
+    @FXML
+    public void checkAvailableStocks()
+    {
+
+    }
+
+    @FXML
+    public void notifyNearestAvailableDonors()
+    {
+
+    }
+
+    @FXML
+    public void sendDonationRequest()
+    {
+
+    }
+
+    @FXML
+    public void  updateDonorMedicalData()
+    {
+
+    }
+
+    @FXML
+    public void cancelDonationRequest()
+    {
+
+    }
+
+    private void initializeDonationRequestAppointmentTable()
+    {
+        // DONATION REQUESTS TAB
+
+        // populate donation requests table
+        // a personnel can only see the requests made at their clinic
+
+        donationRequestDonorFirstNameTableColumn.setCellValueFactory(data ->
+                new SimpleStringProperty(data.getValue().getDonationRequest().getDonor().getFirstName()));
+        donationRequestDonorLastNameTableColumn.setCellValueFactory(data ->
+                new SimpleStringProperty(data.getValue().getDonationRequest().getDonor().getLastName()));
+        donationRequestAppointmentDateTableColumn.setCellValueFactory(data ->
+                data.getValue().appointmentDateProperty());
+        donationRequestAppointmentHourTableColumn.setCellValueFactory(data ->
+                data.getValue().appointmentTimeProperty());
+        // TODO figure this out
+//        donationRequestStatusTableColumn.setCellValueFactory(data ->
+//                data.getValue().getDonationRequest().getStatus());
+
+        List<DonationAppointment> thisPersonnelDonationAppointments =
+                donationAppointmentRepository.getAll()
+                        .stream()
+                        // TODO might need to check the flags from DonationRequest (to see only unhandled requests)
+                        .filter(donationAppointment -> donationAppointment.getClinic() == currentPersonnel.getClinic())
+                        .collect(Collectors.toList());
+
+        ObservableList<DonationAppointment> thisPersonnelDonationAppointmentsObservableList =
+                FXCollections.observableArrayList(thisPersonnelDonationAppointments);
+
+        donationRequestsTableView.setItems(thisPersonnelDonationAppointmentsObservableList);
+    }
+
+    private void initializeBloodRequestsTable()
+    {
+        // TODO wait for Tatiana to add some attributes to class BloodRequest
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
+        // populate table views
+
+        // DONATION REQUESTS TAB
+        initializeDonationRequestAppointmentTable();
+        // TODO add event listeners when selecting a row (if needed)
+
+        // BLOOD REQUESTS TAB
+        initializeBloodRequestsTable();
+
+        // AVAILABLE STOCKS TAB
+
+
+        // BLOOD CONTAINER JOURNEY TAB
+
+
 
     }
 }
